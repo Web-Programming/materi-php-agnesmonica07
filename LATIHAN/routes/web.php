8acb1e51976d9492714bd89da7cfe1f32f5a32a2
agnesmonica07 <agnesmonica071205@gmail.com>
2025-04-29 20:35:56 +0700

    membuat view dan atur routing

--- LATIHAN/routes/web.php
+++ LATIHAN/routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('beranda',
+    ['name' => 'Agnes Monica','email' => 'agnesmonica071205@gmail.com', 'address' => 'jln perindustrian 2 Griya Villa Sukarami']
+    );
+});
+
+Route::get('/berita/{id}/{judul?}', function ($id, $judul = null) {
+    return view('berita',
+    ['id' => $id, 'judul' => $judul]
+    );
+});
