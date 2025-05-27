9905c77f7481a279adaf297c681b62c8fb2f8a47
agnesmonica07 <agnesmonica071205@gmail.com>
2025-05-27 19:35:51 +0700

    CRUD dan File Upload

--- LATIHAN/routes/web.php
+++ LATIHAN/routes/web.php
@@ -7,4 +7,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view(
+        'beranda',
+        [
+            'name' => 'Agnes Monica',
+            'email' => 'agnesmonica071205@gmail.com',
+            'alamat' => 'Palembang'
+        ]
+    );
+});
+
+Route::get('/berita/{id}/{judul?}', function ($id, $judul = null) {
+    return view('berita', ['id' => $id, 'judul' => $judul]);
+});
+
+//membuat route ke halam prodi index melalui controller ProdiController
+Route::get('/prodi/index', [ProdiController::class, 'index']);
+
 Route::resource('prodi', ProdiController::class);
+Route::get('/prodi/create', [ProdiController::class, 'create']);
+Route::get('prodi/{id}', [ProdiController::class, 'show']);
